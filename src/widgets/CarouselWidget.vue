b3f72c9ba7ccaca892a3a0f8c459cbff497b63d9
DiiegoAG <diego@apdevs.com>
2024-07-09 16:32:50 -0600

    Se crea widget carrusel para reutilizarlo

---
+++ src/widgets/CarouselWidget.vue
@@ -0,0 +1,34 @@
+<template>
+    <Splide :options="{ rewind: true, width: widht, height: height, pagination: false }" aria-label="My Favorite Images">
+        <SplideSlide v-for="(imgName, index) in gallery" :key=index>
+            <img :src="require(`@/assets/${imgName}`)" alt="imgName" data-position="top center" />
+        </SplideSlide>
+    </Splide>
+</template>
+
+<script>
+    import { Splide, SplideSlide } from '@splidejs/vue-splide';
+    import '@splidejs/vue-splide/css';
+    //To watch slide options visit https://splidejs.com/guides/options/#options
+
+    export default {
+        props: {
+            gallery: {
+                default: function () { return ['pic01.jpg', 'pic02.jpg', 'pic03.jpg']},
+                type: Array
+            },
+            widht: {
+                default: '450px',
+                type: String
+            },
+            height: {
+                default: '450px',
+                type: String
+            }
+        },
+        components: {
+            Splide,
+            SplideSlide
+        }
+    }
+</script>
